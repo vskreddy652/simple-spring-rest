cd51691f1a22ebee841a360ee76cb56c27f187c2
unknown <vskreddy652@yahoo.com>
2019-05-04 00:19:55 +0530

    initial commit

---
+++ src/main/java/com/example/demo/SimpleRestSpringBootApplication.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class SimpleRestSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleRestSpringBootApplication.class, args);
+	}
+
+}
+
+@RestController
+class Abcd
+{
+	@GetMapping("/")
+	public String sayHello()
+	{
+		return "Saying Hello";
+	}
+}
